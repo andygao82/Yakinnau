c3702a2d268ab2d397baa2f08f5a210a84382867
Andy Gao <freedomandy82@gmail.com>
2023-10-31 21:26:55 +1100

    wip

--- wp-content/themes/yakinau/js/global.js
+++ wp-content/themes/yakinau/js/global.js
@@ -1,3 +1,14 @@
 document.addEventListener( 'DOMContentLoaded', function () {
-  console.log(123)
-} );
+  const siteWrapper = document.getElementById('wrapper');
+  setTimeout (function () {
+    siteWrapper.classList.add('active');
+    console.log(siteWrapper.classList);
+  }, 2000)
+  setTimeout (function () {
+    siteWrapper.classList.add('bg-done');
+  }, 3000)
+  window.addEventListener('scroll', function () {
+    const scrollValue = window.scrollY;
+    siteWrapper.style.backgroundPositionY = 'calc(50% + '+ scrollValue * .2 + 'px)';
+  });
+});
